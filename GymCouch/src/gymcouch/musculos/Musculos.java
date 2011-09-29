b1373baf246c3e76929790e7745bbb49f2a70a60
ed.pimentel.f@gmail.com <ed.pimentel.f@gmail.com@1c9a1b78-30b6-403e-5938-ae80cdefabc2>
2011-09-29 22:41:43 +0000

    Falta perfil, agregar la funcionalidad de los botones de los musculos y agregar los ejercicios restantes

--- GymCouch/src/gymcouch/musculos/Musculos.java
+++ GymCouch/src/gymcouch/musculos/Musculos.java
@@ -2,7 +2,11 @@ package gymcouch.musculos;
 
 import gymcouch.main.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class Musculos extends Activity{
 
@@ -10,5 +14,15 @@ public class Musculos extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.musculos);
+		
+		((ImageButton)findViewById(R.id.ibtnBiceps)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intencion = new Intent(getBaseContext(), Musculo.class);
+				intencion.putExtra("musculoSeleccionado", "biceps");
+				startActivity(intencion);
+			}
+		});
 	}
 }
