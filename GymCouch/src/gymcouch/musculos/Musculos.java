e2037beb89dfeda8c60a535cf76c870449524aa6
gurrusquietaqda <gurrusquietaqda@1c9a1b78-30b6-403e-5938-ae80cdefabc2>
2011-09-29 17:35:44 +0000

    Falta perfil e imagenes de los musculos

---
+++ GymCouch/src/gymcouch/musculos/Musculos.java
@@ -0,0 +1,14 @@
+package gymcouch.musculos;
+
+import gymcouch.main.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Musculos extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.musculos);
+	}
+}
